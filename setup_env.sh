404e9020f7a18c7bb5d2d117cac4d006a2a4efea
Alan Iwi <alan.iwi@stfc.ac.uk>
2020-06-09 18:50:07 +0100

    allow for a setup_env_local.sh

--- setup_env.sh
+++ setup_env.sh
@@ -1,3 +1,11 @@
+thisdir=$(dirname $BASH_SOURCE)
+local_script=$thisdir/setup_env_local.sh
+if [ -e $local_script ]
+then
+    . $local_script
+fi
+
+
 export DJANGO_SETTINGS_MODULE=index_monitor.settings
 
 if [ -z "$PYTHONPATH" ]
